31cb28b6c66732710f02d55026f00ef901c8993a
AriDevK <AriadneFave@hotmail.com>
2023-11-02 21:29:15 -0500

    configure typescript, es un horror el autocompletado para javascript dios mio

--- index.ts
+++ index.ts
@@ -1,11 +1,15 @@
 const express = require('express');
 const { faker } = require('@faker-js/faker');
 
-const app = express();
+import { User } from "./models/user.model";
+import { Application, Request, Response } from 'express';
+
+
+const app: Application = express();
 const port = 3000;
 
 
-let mock =[]
+let mock: User[] = []
 for (let i = 1; i <= 100; i++) {
     mock.push({
       id: i,
@@ -18,7 +22,7 @@ for (let i = 1; i <= 100; i++) {
 }
 
 
-app.get('/users', (req, res) => {
+app.get('/users', (req: Request, res: Response) => {
   const { limit, offset } = req.query;
 
   if (limit && offset) {
@@ -30,9 +34,9 @@ app.get('/users', (req, res) => {
   }
 })
 
-app.get('/users/:id', (req, res) => {
+app.get('/users/:id', (req: Request, res: Response) => {
   let { id } = req.params;
-  let data = mock.filter(pokemon => pokemon.id == id)
+  let data = mock.filter(user => user.id == id)
   let response = data.length > 0 ? data[0] : {}
 
   res.json(response)
---
+++ models/user.model.ts
@@ -0,0 +1,8 @@
+export interface User {
+  id: number;
+  name: string;
+  email: string;
+  phone: string;
+  image: string;
+  biography: string;
+}
