651abf37cf83b42c3009bc51195fcf20b88325e8
AriDevK <AriadneFave@hotmail.com>
2023-11-02 21:14:21 -0500

    se agrego faker (en el fork seguro) para crear un mock de datos falsos

--- index.js
+++ index.js
@@ -1,28 +1,24 @@
 const express = require('express');
+const { faker } = require('@faker-js/faker');
 
 const app = express();
 const port = 3000;
 
 
-let mock =[
-  {
-    id: 1,
-    name: 'Bulbasaur',
-    type: 'grass'
-  },
-  {
-    id: 2,
-    name: 'Charmander',
-    type: 'fire'
-  },
-  {
-    id: 3,
-    name: 'Squirtle',
-    type: 'water'
-  }
-]
+let mock =[]
+for (let i = 1; i <= 100; i++) {
+    mock.push({
+      id: i,
+      name: faker.person.fullName(),
+      email: faker.internet.email(),
+      phone: faker.phone.number(),
+      image: faker.image.avatar(),
+      biography: faker.person.bio(),
+    })
+}
 
-app.get('/pokemons', (req, res) => {
+
+app.get('/users', (req, res) => {
   const { limit, offset } = req.query;
 
   if (limit && offset) {
@@ -34,7 +30,7 @@ app.get('/pokemons', (req, res) => {
   }
 })
 
-app.get('/pokemons/:id', (req, res) => {
+app.get('/users/:id', (req, res) => {
   let { id } = req.params;
   let data = mock.filter(pokemon => pokemon.id == id)
   let response = data.length > 0 ? data[0] : {}
